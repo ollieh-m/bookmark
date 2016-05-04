bff6c1004d49c31e801cad1f7d8efb1c4a3d23e1
Ben Stein <bumstain@gmail.com>
2016-05-05 02:06:37 +1000

    Challenge 13: Configured the rack environment and added development database

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,15 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
   end
+
   get '/links/new' do
   	erb :'links/new'
   end
