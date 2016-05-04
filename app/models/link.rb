1161f8e310690a02feb91cf015841533eedf79c2
hnamitha1 <namitha.h@gmail.com>
2016-05-04 17:26:59 +0100

    gemlokfile

--- app/models/link.rb
+++ app/models/link.rb
@@ -11,6 +11,6 @@ class Link
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, "postgres://namitha:root@localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
