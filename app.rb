6b4572579fd3dbb330020436489819644c475403
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-05-04 14:23:30 +0200

    ch11

--- app.rb
+++ app.rb
@@ -2,13 +2,20 @@ require 'sinatra/base'
 require './app/models/bookmark'
 
 class BookmarkManager < Sinatra::Base
+
   get '/' do
     @bookmarks = Bookmark.all
     erb :index
   end
 
+  get '/bookmarks/new' do
+    erb :'bookmark_new'
+  end
 
-
+  post '/add_bookmark' do
+    Bookmark.create(name: params[:title], url: params[:url])
+    redirect '/'
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
---
+++ spec/features/add_link_spec.rb
@@ -0,0 +1,11 @@
+
+feature "add bookmarks" do
+  scenario "user can add another bookmarks" do
+    visit('/')
+    click_link('Add Link')
+    fill_in('title', :with => 'Sample Title')
+    fill_in('url', :with => 'http://www.sample.org')
+    click_button('Save')
+    expect(page).to have_content "Sample Title"
+  end
+end
