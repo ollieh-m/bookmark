357680167c8d82ac2eeddac7fd5a36178f68c45e
ollieh-m <ollie.hm@cantab.net>
2016-05-05 15:44:21 +0100

    Challenge 18 - test user signup

---
+++ spec/features/user_signup_spec.rb
@@ -0,0 +1,12 @@
+feature 'user signup' do
+
+  scenario 'successfully signing up a user' do
+    visit('/')
+    fill_in('email', with: 'user@example.com')
+    fill_in('password', with: 'password1')
+    click_button('Sign up')
+    expect(User.all.count).to eq 1
+    expect(page).to have_content('Welcome to your bookmark manager!')
+  end
+
+end
